e91fd1bd54f8b3184244ec0b4e00822150c4d9ae
asbsoft <ab2014box@gmail.com>
2018-01-25 19:51:49 +0200

    fix bug in migration windows bat-file: pass wrong parameter

--- migrations/migrate.bat
+++ migrations/migrate.bat
@@ -12,6 +12,6 @@ goto DONE
 :START
 set CMD=up
 if not '%1'=='' set CMD=%1
-call %APP% migrate/%CMD% --migrationPath=. %2 %3 %4 %5 %6 %7 %8 %9
+call %APP% migrate/%CMD% "--migrationPath=." %2 %3 %4 %5 %6 %7 %8 %9
 :DONE
 
